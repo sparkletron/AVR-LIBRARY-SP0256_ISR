585e1fc6ac150fada88bf4ad0800a1c38f5c8382
Jay Convertino <electrobs@gmail.com>
2024-03-11 18:47:33 -0400

    change license

--- README.md
+++ README.md
@@ -5,27 +5,6 @@ Speech without busy wait (ISR), using SP0256
 
 This library requires the commonDefines.h header which is located in the ATMEGA328P-LIBRARY-Common_Defines repository.
 
-This code is licensed under the following GNU GPL V2 (also same notice in all source code):
-
-    atmel328P ISR based method for SP0256 speech chip
-    Copyright (C) July 17th, 2014  John Convertino
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-This will be a C library usuable in AVR Studio, and the generic AVR compiler.
-
 ## Example
 ```c
 #include <inttypes.h>
--- src/SPO256.c
+++ src/SPO256.c
@@ -1,31 +1,38 @@
-/*
-ISR.c
-
-atmega328P SP0256 using ISR
-
-Copyright (C) July 17, 2014 John Convertino
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
- *  Created on: Jul 17, 2014
- *      Author: John Convertino
- *      Contact: electrobs@gmail.com
+/*******************************************************************************
+ * @file    SPO256.c
+ * @author  Jay Convertino(electrobs@gmail.com)
+ * @date    2014.07.17
+ * @brief   atmega328P SP0256 using ISR
+ * @detail  This implementation allows the SP0256 speech chip to use a interrupt to get the next allophone.
+ *          Allowing the microcontroller to be free to do other tasks. Works by using a pin interrupt so
+ *          any IO can be used for monitoring LRQ. Does use ALD to pulse in data.
+ * @version 0.6.0
  *
- *      Version 1.0
- *      July 17th, 2014: Started, and completed.
- */
+ * @TODO
+ *  - Cleanup interface
+ *
+ * @license mit
+ *
+ * Copyright 2024 Johnathan Convertino
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
 
 //Standard AVR includes
 #include <avr/io.h>
